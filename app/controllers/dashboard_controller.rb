33c268d8572f9dfdcbd3b8e4fa1fa451b83549d8
Jake O'Donnell <jakeod99@gmail.com>
2019-12-09 13:49:24 -0500

    dashboard page recognizes patient

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,22 @@
 class DashboardController < ApplicationController
   def index
+    pat_param = params[:patient]
+    redirect_to :root && return if pat_param.blank?
+
+    patients = Rails.cache.read("patients")
+    if patients.nil?
+      @patient = SessionHandler.fhir_client(session.id).read(FHIR::Patient, pat_param).resource
+    else
+      @patient = patients.select{ |patient| patient.id.eql?(pat_param) }.first
+    end
+
+    redirect_to :root && return if @patient.blank?
+
+    patient_name = @patient.name.select{ |name| name.use.eql?("official") }.first
+
+    @patient_name_first = patient_name.given.first
+    @patient_name_middle = patient_name.given[1..-1].join(" ")
+    @patient_name_middle ||= ""
+    @patient_name_last = patient_name.family
   end
 end
